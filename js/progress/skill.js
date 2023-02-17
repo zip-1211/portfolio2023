f0428a16982f99ed9fdc9bfd866fde784da240b4
jeong hyeon kim <124849040+zip-1211@users.noreply.github.com>
2023-02-17 16:17:30 +0900

    Add files via upload

---
+++ js/progress/skill.js
@@ -0,0 +1,13 @@
+(function ($) {
+  "use strict";
+
+  // Skills section
+  $('#skills').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, { offset: '80%'} );
+
+
+})(jQuery);
+
